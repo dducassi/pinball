e318bd9d1d9d237daa66aab364d741bf32c1656a
dducassi <dducassi@gmail.com>
2026-03-12 21:40:21 +0700

    Integrate TableBuilder, remove hardcoded table gen
    - Import TableBuilder and instantiate in __init__
    - Replace direct gen_obs/gen_blocks calls with builder methods
    - Remove old gen_obs and gen_blocks methods

--- pinball.py
+++ pinball.py
@@ -13,6 +13,7 @@ from blocks import Block
 from flipper import Flipper
 from gamestate import GameState
 from physics_engine import PhysicsEngine
+from table_builder import TableBuilder
 
 
 import os
@@ -77,67 +78,16 @@ class Pinball:
         self.obs = []
         self.blocks = [] 
         self.flippers = [self.fl, self.fr]
-        self.gen_obs()
-        self.gen_blocks()
+        self.table_builder = TableBuilder(self.settings)
+        self.obs = self.table_builder.generate_obstacles()
+        self.blocks = self.table_builder.generate_blocks()
         self.physics_engine = PhysicsEngine(self.b, self.flippers, self.obs, self.blocks, self.settings)
-
-    def gen_obs(self):
-        x = self.settings.screen_width / 2
-        y = (self.settings.screen_height) / 5
-        c = self.settings.blu
-        self.obs.append(Obstacle(x, y, c))
-        x = 0
-        y = 0
-        c = self.settings.blu
-        self.obs.append(Obstacle(x, y, c))
-        x = self.settings.screen_width
-        y = 0
-        c = self.settings.blu
-        self.obs.append(Obstacle(x, y, c))
-        x = .22 * self.settings.orad
-        y = 6/7 * (self.settings.screen_height)
-        c = self.settings.blu
-        self.obs.append(Obstacle(x, y, c))
-        x = self.settings.screen_width - .22 * self.settings.orad
-        y = 6/7 * (self.settings.screen_height)
-        c = self.settings.blu
-        self.obs.append(Obstacle(x, y, c))
-
-
 
 
     def draw_obs(self):
         for ob in self.obs:
             pygame.draw.circle(self.screen, ob.c, (ob.x, ob.y), self.settings.orad)
     
-    def gen_blocks(self):
-
-        # Left lower block
-        x1 = 0
-        y1 = self.settings.screen_height
-        x2 = 0
-        y2 = self.settings.screen_height - (3/14) * self.settings.screen_height
-        x3 = (2/7) * self.settings.screen_width
-        y3 = self.settings.screen_height - (1/14) * self.settings.screen_height
-        x4 = (2/7) * self.settings.screen_width
-        y4 = self.settings.screen_height
-        c = self.settings.gry
-        self.blocks.append(Block(x1, y1, x2, y2, x3, y3, x4, y4, c))
-        #[(0, self.settings.screen_height), (100, self.settings.screen_height), (0, self.settings.screen_height - 120), (100, self.settings.screen_height - 20)]
-
-        # Right lower block
-        x1 = self.settings.screen_width
-        y1 = self.settings.screen_height
-        x2 = self.settings.screen_width
-        y2 = self.settings.screen_height - (3/14) * self.settings.screen_height
-        x3 = self.settings.screen_width - (2/7) * self.settings.screen_width 
-        y3 = self.settings.screen_height - (1/14) * self.settings.screen_height
-        x4 = self.settings.screen_width - (2/7) * self.settings.screen_width 
-        y4 = self.settings.screen_height
-
-        c = self.settings.gry
-        self.blocks.append(Block(x1, y1, x2, y2, x3, y3, x4, y4, c))
-
     def draw_blocks(self):
         for block in self.blocks:
             pygame.draw.polygon(self.screen, block.c, [(block.x1, block.y1), (block.x2, block.y2), (block.x3, block.y3), (block.x4, block.y4)])
