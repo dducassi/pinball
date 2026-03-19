7fd46d1a4b7df8eb31e94e79b5428da546d03ac9
dducassi <dducassi@gmail.com>
2026-03-20 04:21:58 +0700

    Allow passing bumper radius

--- bumpers.py
+++ bumpers.py
@@ -5,11 +5,12 @@ import random
 from settings import Settings
 
 class Bumper:
-    def __init__(self, x, y, c):
+    def __init__(self, x, y, c, radius):
 
         self.x = x
         self.y = y
         self.c = c
+        self.radius = radius 
     
     def draw(self):
-        pygame.draw.circle(self.screen, self.c, (self.x, self.y), self.settings.orad)
+        pygame.draw.circle(self.screen, self.c, (self.x, self.y), self.radius)
