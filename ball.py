3edf28360f07fcfdf9a030c1fc6073ef8baeefa9
dducassi <dducassi@gmail.com>
2026-03-21 02:28:39 +0700

    incorporate pinball image

--- ball.py
+++ ball.py
@@ -13,7 +13,7 @@ else:
 
 
 class Ball:
-    def __init__(self, bb_game):
+    def __init__(self, bb_game, image=None):
         self.screen = bb_game.screen
         self.settings = bb_game.settings
         self.screen_rect = bb_game.screen.get_rect()
@@ -22,7 +22,24 @@ class Ball:
         self.launched = False
         self.trapped = False
         self.lost = False
-        self.reset()
+        self.image = image
+        if image:
+            size = int(self.settings.br * 2)
+            self.pinball_image = pygame.transform.scale(image, (size, size))
+            print(f"Ball image scaled to {size}x{size}")
+        else:
+            print("Ball image not provided")
+            self.pinball_image = None
+
+    def draw_ball(self):
+        if self.pinball_image:
+            rect = self.pinball_image.get_rect(center=(int(self.x), int(self.y)))
+            self.screen.blit(self.pinball_image, rect)
+            # Optional debug outline:
+            # pygame.draw.rect(self.screen, (255,0,0), rect, 1)
+        else:
+            pygame.draw.circle(self.screen, self.settings.slv,
+                               (int(self.x), int(self.y)), self.settings.br)
 
     def reset(self):
         if self.lane_x_center is not None and self.lane_bottom is not None:
@@ -44,13 +61,13 @@ class Ball:
         # Friction (from original)
         self.dy += (0.0008 * abs(self.dy)) / self.settings.phys_runs
         if self.dx > 0:
-            self.dx -= (0.00025 * abs(self.dy)) / self.settings.phys_runs
+            self.dx -= (0.00027 * abs(self.dy)) / self.settings.phys_runs
         elif self.dx < 0:
-            self.dx += (0.00025 * abs(self.dy)) / self.settings.phys_runs
+            self.dx += (0.00027 * abs(self.dy)) / self.settings.phys_runs
 
         # Gravity
         if self.y <= self.settings.screen_height:
-            self.dy += 0.027 / self.settings.phys_runs
+            self.dy += 0.025 / self.settings.phys_runs
 
         # Speed cap
         speed = math.hypot(self.dx, self.dy)
@@ -65,7 +82,3 @@ class Ball:
 
         self.x += self.dx
         self.y += self.dy
-
-    def draw_ball(self):
-        pygame.draw.circle(self.screen, self.settings.slv,
-                           (int(self.x), int(self.y)), self.settings.br)
