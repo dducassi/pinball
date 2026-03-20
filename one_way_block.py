312dcca22dab1e635ad28206460666b86da9c8f0
dducassi <dducassi@gmail.com>
2026-03-21 03:18:57 +0700

    Add image parameter

--- one_way_block.py
+++ one_way_block.py
@@ -2,9 +2,9 @@ import math
 from blocks import Block
 
 class OneWayBlock(Block):
-    def __init__(self, vertices, color, restitution=0.8, direction='left'):
-        super().__init__(vertices, color, restitution)
-        self.direction = direction  # 'left' means allow leftward movement
+    def __init__(self, vertices, color, restitution=0.8, direction='left', image=None):
+        super().__init__(vertices, color, restitution, image)
+        self.direction = direction
 
     def collide(self, ball_x, ball_y, ball_dx, ball_dy, ball_radius):
         new_x, new_y, new_dx, new_dy, hit = super().collide(
