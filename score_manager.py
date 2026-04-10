07750d6d0c52358b70a270a31f410d862c53c344
dducassi <dducassi@gmail.com>
2026-04-11 03:33:08 +0700

    Detach color cycling from score manager

--- score_manager.py
+++ score_manager.py
@@ -6,6 +6,7 @@ class ScoreManager:
         self.notification_center.add_observer('bumper_hit', self.on_bumper_hit)
 
     def on_bumper_hit(self, bumper):
+        # Scoring logic
         if bumper.radius > 20:
             self.score += self.settings.pph * 10
         elif 5.5 < bumper.radius < 15:
@@ -13,21 +14,7 @@ class ScoreManager:
         elif bumper.radius < 6:
             self.score += self.settings.pph * 3
         else:
-            self.score += self.settings.pph //2
-
-        # Cycle bumper color (only for bumpers with radius > 5)
-        if bumper.radius > 5:
-            if bumper.color == self.settings.red:
-                new_color = self.settings.blu
-            elif bumper.color == self.settings.blu:
-                new_color = self.settings.wht
-            else:  # white
-                new_color = self.settings.red
-            bumper.color = new_color
-
-            # If it's a large bumper, notify others to sync colors
-            if bumper.radius > 20:
-                self.notification_center.post_notification('large_bumper_hit', new_color)
+            self.score += self.settings.pph // 2
 
         self.notification_center.post_notification('score_changed', self.score)
 
